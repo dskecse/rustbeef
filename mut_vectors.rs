2396bae65264c440112a0d50f0cd3660c399bbd0
Dzianis Dashkevich <dskecse@gmail.com>
2014-01-16 15:46:36 +0300

    Change an element of a vector

--- mut_vectors.rs
+++ mut_vectors.rs
@@ -1,7 +1,7 @@
 fn main() {
     let a_vector = ~[1, 2, 3];
-    let mut another_vector = ~[];
-    another_vector.push_all(a_vector);
+    let mut mut_vector = a_vector;
+    mut_vector[0] = 5;
 
-    println!("The first number is {:d}.", another_vector[0]);
+    println!("The first number is {:d}.", mut_vector[0]);
 }
